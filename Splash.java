3bbff73b58696a49133d2d08fe3416092246c87a
Utkarsh <141557962+UtkarshBhardwaj1@users.noreply.github.com>
2024-01-30 21:58:40 +0530

    Update Splash.java

--- Splash.java
+++ Splash.java
@@ -1,38 +1,46 @@
-package travel.management.system;
-import javax.swing.*;
 import java.awt.*;
-public  class Splash extends JFrame implements Runnable{
-    Thread thread;
-    Splash(){
-        //making frame
-        //setSize(1100,700);
-        setVisible(true);
-//        setLocation(70,30);
-        ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("icons/splash.jpg"));
-        Image i2=i1.getImage().getScaledInstance(1100,700,Image.SCALE_DEFAULT);
-        ImageIcon i3=new ImageIcon(i2);
-        JLabel image=new JLabel(i3);
-        add(image);
-        thread=new Thread(this);
-        thread.start();
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Splash {
+    public static void main(String[] args){
+        SplashFrame f1 = new SplashFrame();
+        f1.setVisible(true);
+        int i;
+        int x=1;
+        for(i=2; i<=600; i+=10, x+=7){
+            f1.setLocation(900 - ((i+x)/2), 500 - (i/2));
+            f1.setSize(i+x,i);
+            try{
+                Thread.sleep(10);
+            }catch(Exception e){}
+        }
         
+    }
+}
+class SplashFrame extends JFrame implements Runnable{
+    Thread t1;
+    SplashFrame(){
+        setLayout(new FlowLayout());
+        ImageIcon c1 = new ImageIcon(ClassLoader.getSystemResource("image"));
+        Image i1 = c1.getImage().getScaledInstance(1030, 750,Image.SCALE_DEFAULT);
+        ImageIcon i2 = new ImageIcon(i1);
+        
+        JLabel l1 = new JLabel(i2);
+        add(l1);
+        setUndecorated(true);
+        t1 = new Thread(this);
+        t1.start();
     }
     public void run(){
         try{
-            Thread.sleep(10000);
-            setVisible(false);
+            Thread.sleep(7000);
+            this.setVisible(false);
             
+            Login l = new Login();
+            l.setVisible(true);
         }catch(Exception e){
-            
-        }
-    }
-    public static void main (String []args){
-       Splash frame= new Splash();
-       int x=1; 
-       for (int i=1;i<=500;x+=7,i+=3){
-            frame.setLocation(625-(x+1)/2, 350-(i/2));
-            frame.setSize(x,i);
-            
+            e.printStackTrace();
         }
     }
 }
